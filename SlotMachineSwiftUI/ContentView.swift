1882b729bed7567b1d945143e1e77662523a58bd
Veronika <shuichi4@gmail.com>
2020-01-09 02:52:21 -0500

    Initial Commit

---
+++ SlotMachineSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotMachineSwiftUI
+//
+//  Created by Veronika Kotckovich on 1/9/20.
+//  Copyright © 2020 centennial. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
